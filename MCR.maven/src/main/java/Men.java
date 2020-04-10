077554ca8e3f955d2b4cdda3c5917a6c753a8f12
unbound1994 <unbound1990@mail.ru>
2020-04-10 13:13:45 +0300

    first commit

---
+++ MCR.maven/src/main/java/Men.java
@@ -0,0 +1,10 @@
+public class Men {
+
+    public static int min = 1;
+    public static int max = 4;
+    public static int maxrun = 100;
+
+    public static String Name = "Вася";
+    public static int Run = min + (int) (Math.random() * maxrun);
+    public static int Jump = min + (int) (Math.random() * max);
+}
---
+++ MCR.maven/src/main/java/Treadmill.java
@@ -0,0 +1,5 @@
+public class Treadmill {
+    public static int min = 1;
+    public static int max = 200;
+    public static int Lenght = min + (int) (Math.random() * max);
+}
